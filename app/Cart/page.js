518e510d7d87627bbe8167a402e90daab9b84a7d
Omar Samir <omar@example.com>
2025-06-08 15:19:56 +0300

    omar

--- app/Cart/page.js
+++ app/Cart/page.js
@@ -1,11 +1,8 @@
-'use client';
 import CartContainer from "../components/CartContainer/CartContainer";
-
-
-// export const metadata = {
-//   title: "User Cart",
-//   description: "Cart items and Option To Make order",
-// };
+export const metadata = {
+  title: "User Cart",
+  description: "Cart items and Option To Make order",
+};
 
 export default function CartPage() {
   return (
